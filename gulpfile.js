2a419d2e5a943288b34eab8d87bf895019c5eeaa
aaron <a.aa.akio.aaron@gmail.com>
2015-02-15 16:32:11 -0500

    Build - deploy triggers dev build first

--- gulpfile.js
+++ gulpfile.js
@@ -61,6 +61,22 @@ gulp.task('templates', function () {
       .pipe(connect.reload())
 })
 
+gulp.task('js', function buildChromeJs() {
+  return browserify('./app/js/index.js').bundle()
+    // log errors
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    // perform bundle
+    .pipe(source('bundle.js'))
+    // .pipe(streamify(uglify()))
+    .pipe(gulp.dest('./dist/'))
+})
+
+gulp.task('html', function devHtml() {
+  return gulp.src('./app/index.html')
+    .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload())
+})
+
 // development
 
 gulp.task('live-dev', ['dev', 'dev-server'], function() {
@@ -68,13 +84,15 @@ gulp.task('live-dev', ['dev', 'dev-server'], function() {
   gulp.watch('./app/images/**', ['img'])
   gulp.watch('./app/js/**', ['live-js'])
   gulp.watch('./app/html/**', ['templates'])
-  gulp.watch('./**/*.html', ['dev-html'])
+  gulp.watch('./app/index.html', ['html'])
   gutil.log(gutil.colors.bgGreen('Watching for changes...'))
 })
 
 gulp.task('dev', function(callback){
-  runSequence('clean', ['live-js', 'templates', 'css', 'img', 'dev-html'], callback)
+  runSequence('clean', 'build-dev', callback)
 })
+
+gulp.task('build-dev', ['js', 'templates', 'css', 'img', 'html'])
 
 gulp.task('dev-server', function startServer() {
   connect.server({
@@ -83,10 +101,11 @@ gulp.task('dev-server', function startServer() {
   })
 })
 
+var bundler = watchify(browserify('./app/js/index.js', watchify.args))
+// brfs needed for watchify
+bundler.transform('brfs')
+
 gulp.task('live-js', function devJs() {
-  var bundler = watchify(browserify('./app/js/index.js', watchify.args))
-  // brfs needed for watchify
-  bundler.transform('brfs')
   return bundler.bundle()
     // log errors if they happen
     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
@@ -101,17 +120,10 @@ gulp.task('live-js', function devJs() {
     .pipe(connect.reload())
 })
 
-gulp.task('dev-html', function devHtml() {
-  return gulp.src('./app/index.html')
-    .pipe(gulp.dest('./dist/'))
-    .pipe(connect.reload())
-})
-
-
 // chrome packaged app
 
 // not optimized but it works
-// still need to hit refresh on extensions page
+// still need to hit refresh on chrome extensions page
 gulp.task('live-chrome', ['build-chrome', 'dev-server'], function liveChrome() {
   gulp.watch('./app/css/**', ['chrome-package'])
   gulp.watch('./app/images/**', ['chrome-package'])
@@ -130,24 +142,7 @@ gulp.task('chrome-meta', function buildChromeMeta() {
     .pipe(gulp.dest('./dist/'))
 })
 
-gulp.task('chrome-js', function buildChromeJs() {
-  return browserify('./app/js/index.js').bundle()
-    // log errors
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    // perform bundle
-    .pipe(source('bundle.js'))
-    // .pipe(streamify(uglify()))
-    .pipe(gulp.dest('./dist/'))
-})
-
-gulp.task('chrome-html', ['templates', 'chrome-js', 'css', 'img'], function buildChromeHtml() {
-  return fs.createReadStream('./app/index.html')
-    // name it, build it
-    .pipe(source('index.html'))
-    .pipe(gulp.dest('./dist/'))
-})
-
-gulp.task('chrome-package', ['chrome-meta', 'chrome-html'], function buildChromePackage(callback) {
+gulp.task('chrome-package', ['chrome-meta', 'templates', 'js', 'css', 'img', 'html'], function buildChromePackage(callback) {
   var packagePath = join(__dirname, 'dist')
   var destPath = join(__dirname, 'dist')
   var keyPath = join(__dirname, 'chrome.pem')
@@ -167,7 +162,7 @@ gulp.task('chrome-package', ['chrome-meta', 'chrome-html'], function buildChrome
 
 // deploy
 
-gulp.task('deploy', function () {
+gulp.task('deploy', ['dev'], function () {
   return gulp.src('./dist/**/*')
     .pipe(deployGh({
       remote: 'origin',
