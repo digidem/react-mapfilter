5afdb513af7f4167ff4a1866ba652af1add3b092
James Halliday <mail@substack.net>
2016-04-09 14:45:30 -0400

    move http server into the app

--- app.js
+++ app.js
@@ -18,6 +18,28 @@ var userDataPath = app.getPath('userData')
 //   console.log('tile server listening on :', server.address().port)
 // })
 
+var http = require('http')
+var fs = require('fs')
+var files = {
+  odk: '/home/substack/projects/test-data/simpleodk.json',
+  geo: '/home/substack/projects/test-data/submissions/incidente.geojson'
+}
+
+var server = http.createServer(function (req, res) {
+  console.log(req.method, req.url)
+  if (req.url === '/odk.json') {
+    res.setHeader('content-type', 'text/json')
+    fs.createReadStream(files.odk).pipe(res)
+  } else if (req.url === '/data.geojson') {
+    res.setHeader('content-type', 'text/json')
+    fs.createReadStream(files.geo).pipe(res)
+  } else {
+    res.statusCode = 404
+    res.end('not found\n')
+  }
+})
+server.listen(3210)
+
 app.on('ready', ready)
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
