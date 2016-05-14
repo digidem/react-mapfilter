450cddd3cc3097d24da654c9a409b47152fc0376
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-05-14 16:59:18 -0700

    add react-tap-event-plugin to main entry point

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const App = require('./containers/app')
 
+// Needed by material-ui for onTouchTap to work
+require('react-tap-event-plugin')()
+
 ReactDOM.render(<App />, document.getElementById('root'))
