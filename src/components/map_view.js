85484465c866f6a705b39591893bdc305b5fc98b
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-05-15 18:40:31 -0700

    Colored markers on map

--- src/components/map_view.js
+++ src/components/map_view.js
@@ -15,6 +15,7 @@ require('../../css/popup.css')
 /* Mapbox [API access token](https://www.mapbox.com/help/create-api-access-token/) */
 mapboxgl.accessToken = config.mapboxToken
 
+const COLORS = config.colors
 const DEFAULT_STYLE = 'mapbox://styles/gmaclennan/cio7mcryg0015akm9b6wur5ic'
 
 const emptyFeatureCollection = {
@@ -35,8 +36,9 @@ const pointStyleLayer = {
   source: 'features',
   interactive: true,
   layout: {
-    'icon-image': 'marker-cab2d6',
-    'icon-allow-overlap': true
+    'icon-image': 'marker-{marker-color}',
+    'icon-allow-overlap': true,
+    'icon-offset': [0, -10]
   }
 }
 
@@ -47,8 +49,9 @@ const pointHoverStyleLayer = {
   interactive: false,
   filter: ['==', 'id', ''],
   layout: {
-    'icon-image': 'marker-cab2d6-hover',
-    'icon-allow-overlap': true
+    'icon-image': 'marker-{marker-color}-hover',
+    'icon-allow-overlap': true,
+    'icon-offset': [0, -10]
   }
 }
 
@@ -78,6 +81,35 @@ function lintFeatures (features) {
   return emptyFeatureCollection
 }
 
+function mapFeaturesToData (features, fieldMapping, coloredField) {
+  const colorMapping = {}
+  let colorIndex = 0
+  return {
+    type: 'FeatureCollection',
+    features: features.features.map(feature => {
+      const props = feature.properties
+      let color
+      let coloredFieldValue = get(props, coloredField)
+      if (colorMapping[coloredFieldValue]) {
+        color = colorMapping[coloredFieldValue]
+      } else {
+        color = colorMapping[coloredFieldValue] = COLORS[colorIndex]
+        colorIndex++
+      }
+      return {
+        ...feature,
+        properties: {
+          id: feature.id,
+          img: get(props, fieldMapping.img),
+          title: get(props, fieldMapping.title),
+          subtitle: get(props, fieldMapping.subtitle),
+          'marker-color': color.slice(1)
+        }
+      }
+    })
+  }
+}
+
 /**
  * @private
  * For a given geojson FeatureCollection, return the geographic bounds.
@@ -97,6 +129,7 @@ function getBounds (fc) {
 
 class MapView extends React.Component {
   static defaultProps = {
+    coloredField: 'happening',
     mapStyle: DEFAULT_STYLE,
     onMarkerClick: noop,
     onMove: noop,
@@ -110,6 +143,8 @@ class MapView extends React.Component {
   static propTypes = {
     /* map center point [lon, lat] */
     center: PropTypes.array,
+    /* Field to color features by */
+    coloredField: PropTypes.string,
     /* Geojson FeatureCollection of features to show on map */
     features: PropTypes.object,
     /**
@@ -130,12 +165,7 @@ class MapView extends React.Component {
     ])),
     /**
      * - NOT yet dynamic e.g. if you change it the map won't change
-     * Map style. This must be an an object conforming to the schema described in the [style reference](https://mapbox.com/mapbox-gl-style-spec/), or a URL to a JSON style. To load a style from the Mapbox API, you can use a URL of the form `mapbox://styles/:owner/:style`, where `:owner` is your Mapbox account name and `:style` is the style ID. Or you can use one of the predefined Mapbox styles:
-     * `mapbox://styles/mapbox/basic-v8` - Simple and flexible starting template.
-     * `mapbox://styles/mapbox/bright-v8` - Template for complex custom basemaps.
-     * `mapbox://styles/mapbox/streets-v8` - A ready-to-use basemap, perfect for minor customization or incorporating your own data.
-     * `mapbox://styles/mapbox/light-v8` - Subtle light backdrop for data vizualizations.
-     * `mapbox://styles/mapbox/dark-v8` - Subtle dark backdrop for data vizualizations.
+     * Map style. This must be an an object conforming to the schema described in the [style reference](https://mapbox.com/mapbox-gl-style-spec/), or a URL to a JSON style. To load a style from the Mapbox API, you can use a URL of the form `mapbox://styles/:owner/:style`, where `:owner` is your Mapbox account name and `:style` is the style ID. Or you can use one of the predefined Mapbox styles.
      */
     mapStyle: PropTypes.string,
     /**
@@ -197,12 +227,7 @@ class MapView extends React.Component {
    * @return {string} HTML string
    */
   getPopupHtml (o) {
-    const fieldMapping = this.props.fieldMapping
-    const templateContext = Object.keys(fieldMapping).reduce((prev, field) => {
-      prev[field] = get(o, fieldMapping[field])
-      return prev
-    }, {})
-    return popupTemplate(templateContext)
+    return popupTemplate(o)
   }
 
   render () {
@@ -217,7 +242,7 @@ class MapView extends React.Component {
   // The first time our component mounts, render a new map into `mapDiv`
   // with settings from props.
   componentDidMount () {
-    const { center, filter, mapStyle, features, zoom } = this.props
+    const { center, filter, mapStyle, features, fieldMapping, coloredField, zoom } = this.props
 
     const map = this.map = window.map = new mapboxgl.Map({
       style: mapStyle,
@@ -238,9 +263,8 @@ class MapView extends React.Component {
       map.on('moveend', this.handleMapMoveOrZoom)
       map.on('click', this.handleMapClick)
       map.on('mousemove', this.handleMouseMove)
-      this.featuresSource = new mapboxgl.GeoJSONSource({
-        data: lintFeatures(features)
-      })
+      const layerData = mapFeaturesToData(features, fieldMapping, coloredField)
+      this.featuresSource = new mapboxgl.GeoJSONSource({data: layerData})
       map.addSource('features', this.featuresSource)
       // TODO: Should choose style based on whether features are point, line or polygon
       map.addLayer(pointStyleLayer)
@@ -256,46 +280,75 @@ class MapView extends React.Component {
     }
   }
 
+  componentWillReceiveProps (nextProps) {
+    this.moveIfNeeded(nextProps.center, nextProps.zoom)
+    this.updateDataIfNeeded(
+      nextProps.features,
+      nextProps.fieldMapping,
+      nextProps.coloredField
+    )
+    this.updateFilterIfNeeded(nextProps.filter)
+  }
+
   // We always return false from this function because we don't want React to
   // handle any rendering of the map itself, we do all that via mapboxgl
   shouldComponentUpdate (nextProps) {
-    const mapPosition = {
-      center: this.map.getCenter().toArray(),
-      zoom: this.map.getZoom()
-    }
-    const nextMapPosition = {
-      center: nextProps.center,
-      zoom: nextProps.zoom
-    }
-    const shouldMapMove = nextProps.center && nextProps.zoom &&
-      !deepEqual(mapPosition, nextMapPosition)
-    if (shouldMapMove) {
-      this.map.jumpTo(nextMapPosition)
-    }
-    // If no map center or zoom passed, set map extent to extent of marker layer
-    if (!nextProps.center && !this.props.center || !nextProps.zoom && !this.props.zoom) {
-      this.map.fitBounds(getBounds(nextProps.features), {padding: 15})
-    }
-    if (nextProps.features !== this.props.features) {
-      const features = lintFeatures(nextProps.features)
-      if (this.map.loaded()) {
-        this.featuresSource.setData(features)
-      } else {
-        this.map.on('style.load', () => this.featuresSource.setData(features))
-      }
-    }
-    if (nextProps.filter !== this.props.filter && nextProps.filter) {
-      if (this.map.style.loaded()) {
-        this.map.setFilter('features', nextProps.filter)
-      } else {
-        this.map.on('style.load', () => this.map.setFilter('features', nextProps.filter))
-      }
-    }
     return false
   }
 
   componentWillUnmount () {
     this.map.remove()
+  }
+
+  /**
+   * Moves the map to a new position if it is different from the current position
+   * @param {array} center new coordinates for center of map
+   * @param {number} zoom   new zoom level for map
+   * @return {boolean} true if map has moved, otherwise false
+   */
+  moveIfNeeded (center, zoom) {
+    const currentPosition = {
+      center: this.map.getCenter().toArray(),
+      zoom: this.map.getZoom()
+    }
+    const newMapPosition = {
+      center,
+      zoom
+    }
+    const shouldMapMove = center && zoom &&
+      !deepEqual(currentPosition, newMapPosition)
+    if (shouldMapMove) {
+      console.log('Moving map')
+      this.map.jumpTo(newMapPosition)
+      return true
+    }
+    return false
+  }
+
+  updateDataIfNeeded (features, fieldMapping, coloredField) {
+    if (features === this.props.features &&
+        (!fieldMapping || fieldMapping === this.props.fieldMapping) &&
+        (!coloredField || coloredField === this.props.coloredField)) {
+      return
+    }
+    console.log('updated features')
+    const layerData = mapFeaturesToData(features, fieldMapping, coloredField)
+    if (this.map.loaded()) {
+      this.featuresSource.setData(layerData)
+    } else {
+      this.map.on('style.load', () => this.featuresSource.setData(layerData))
+    }
+  }
+
+  updateFilterIfNeeded (filter) {
+    if (filter !== this.props.filter && filter) {
+      console.log('new filter')
+      if (this.map.style.loaded()) {
+        this.map.setFilter('features', filter)
+      } else {
+        this.map.on('style.load', () => this.map.setFilter('features', filter))
+      }
+    }
   }
 }
 
