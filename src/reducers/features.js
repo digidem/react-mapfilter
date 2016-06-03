77e29e43a786c170a7ba380b1c37cebf18da5a7d
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-06-02 19:34:47 -0700

    Temp: hack to load sample data

--- src/reducers/features.js
+++ src/reducers/features.js
@@ -1,4 +1,19 @@
-const markers = require('../../test/fixtures/markers.json').features
+const fs = require('fs')
+
+/**
+ * Attempt to parse a datestring, returning `false` if it can't be parsed
+ * @param {string} possibleDate [description]
+ * @return {date|boolean} returns a Date object or `false` if not a date.
+ */
+function reviveDate (k, v) {
+  if (typeof v !== 'string') return v
+  var date = new Date(v)
+  if (isNaN(date)) return v
+  return date
+}
+
+const markersJson = fs.readFileSync(__dirname + '/../../test/fixtures/markers.json', 'utf8')
+const markers = JSON.parse(markersJson, reviveDate).features
 
 const features = (state = markers, action) => {
   switch (action.type) {
