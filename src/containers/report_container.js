418c47a2829f1736e91a3018b49d2d6b559635ef
Seth Fitzsimmons <seth@mojodna.net>
2016-12-06 12:01:26 -0800

    Implemented feature filtering

--- src/containers/report_container.js
+++ src/containers/report_container.js
@@ -1,34 +1,40 @@
 const React = require('react')
 const { connect } = require('react-redux')
-const { PropTypes } = React;
+const { PropTypes } = React
 
 const FeatureModal = require('../components/feature_modal')
 const MFPropTypes = require('../util/prop_types')
 const MapView = require('../components/map_view')
+const getFieldMapping = require('../selectors/field_mapping')
+const getFilteredFeatures = require('../selectors/filtered_features')
 const getMapGeoJSON = require('../selectors/map_geojson')
 const getMapBoxFilter = require('../selectors/mapbox_filter')
-const getFieldMapping = require('../selectors/field_mapping')
 
 class ReportContainer extends React.Component {
   static propTypes = {
+    features: PropTypes.array.isRequired,
+    fieldMapping: MFPropTypes.fieldMapping,
+    filter: MFPropTypes.mapboxFilter,
     geojson: PropTypes.shape({
       type: PropTypes.oneOf(['FeatureCollection']).isRequired,
       features: PropTypes.arrayOf(MFPropTypes.mapViewFeature).isRequired
-    }),
-    fieldMapping: MFPropTypes.fieldMapping,
-    filter: MFPropTypes.mapboxFilter,
+    }).isRequired
   }
 
   render () {
+    const { features, fieldMapping, filter, geojson } = this.props
+
     return (
-      <div className="report container" style={{ display: 'flex', flexGrow: 1, flexWrap: 'wrap', overflow: 'scroll' }}>
+      <div className='report container' style={{ display: 'flex', flexGrow: 1, flexWrap: 'wrap', overflow: 'scroll' }}>
         <MapView
+          fieldMapping={fieldMapping}
+          filter={filter}
+          geojson={geojson}
           style={{ height: '500px', width: '100%' }}
-          disableScrollToZoom={true}
-          {...this.props}
+          disableScrollToZoom
         />
         {
-          this.props.geojson.features.map((feature, id) => (
+          features.map((feature, id) => (
             <FeatureModal
               key={id}
               id={feature.properties.id}
@@ -43,6 +49,7 @@ class ReportContainer extends React.Component {
 function mapStateToProps (state) {
   return {
     geojson: getMapGeoJSON(state),
+    features: getFilteredFeatures(state),
     filter: getMapBoxFilter(state),
     fieldMapping: getFieldMapping(state)
   }
