2978bb556a36752b5728197cbdfad125024ce0bb
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-12-05 23:06:00 -0800

    Export as a component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
 const React = require('react')
-const ReactDOM = require('react-dom')
-const App = require('./containers/app')
+const { Provider } = require('react-redux')
+const { createStore, applyMiddleware, compose } = require('redux')
+const thunk = require('redux-thunk').default
+const Router = require('react-router/BrowserRouter').default
+const getMuiTheme = require('material-ui/styles/getMuiTheme').default
+const MuiThemeProvider = require('material-ui/styles/MuiThemeProvider').default
+const {IntlProvider} = require('react-intl-redux')
+const {addLocaleData} = require('react-intl')
+const en = require('react-intl/locale-data/en')
+const es = require('react-intl/locale-data/es')
+
+addLocaleData([...en, ...es])
+
+const IndexRoute = require('./containers/index_route')
+const reducers = require('./reducers')
+const { replaceFeatures } = require('./action_creators')
+
+// Roboto font
+require('../css/fonts.css')
+require('../css/animations.css')
 
 // Needed by material-ui for onTouchTap to work
 require('react-tap-event-plugin')()
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Attach Chrome devTools extensions if it is present.
+const devTools = window.devToolsExtension ? window.devToolsExtension() : undefined
+const storeEnhancer = devTools ? compose(devTools, applyMiddleware(thunk)) : applyMiddleware(thunk)
+
+class MapFilter extends React.Component {
+  constructor (props) {
+    super(props)
+    const initialState = {features: props.features}
+    this.store = createStore(reducers, initialState, storeEnhancer)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.features === this.props.features) return
+    this.store.dispatch(replaceFeatures(nextProps.features))
+  }
+
+  render () {
+    return <Provider store={this.store}>
+      <IntlProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme()}>
+          <Router>
+            {routerProps => <IndexRoute {...routerProps} />}
+          </Router>
+        </MuiThemeProvider>
+      </IntlProvider>
+    </Provider>
+  }
+}
+
+module.exports = MapFilter
--- src/reducers/features.js
+++ src/reducers/features.js
@@ -1,21 +1,4 @@
-const fs = require('fs')
-const path = require('path')
-/**
- * Attempt to parse a datestring, returning `false` if it can't be parsed
- * @param {string} possibleDate [description]
- * @return {date|boolean} returns a Date object or `false` if not a date.
- */
-function reviveDate (k, v) {
-  if (typeof v !== 'string') return v
-  var date = new Date(v)
-  if (isNaN(date)) return v
-  return date
-}
-
-const markersJson = fs.readFileSync(path.join(__dirname, '/../../statics/sample.geojson'), 'utf8')
-const markers = JSON.parse(markersJson, reviveDate).features
-
-const features = (state = markers, action) => {
+const features = (state = [], action) => {
   switch (action.type) {
     case 'ADD_FEATURES':
       return [...state, ...action.payload]
