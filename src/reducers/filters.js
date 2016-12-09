b6c1a081a495ce1adee6df02497a569aac6df002
Gregor MacLennan <gmaclennan@digital-democracy.org>
2016-12-08 23:30:39 -0800

    Add inline docs for filter reducer

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,5 +1,23 @@
 const omit = require('lodash/omit')
 
+/**
+ * Filter state is an object with whose properties are field names
+ * that have filters set, and values are objects whose properties
+ * are expressions, values arrays of expression arguments, e.g.
+ * state.filter = {
+ *   happening: {
+ *     in: ['mining', 'fishing']
+ *   },
+ *   today: {
+ *     '>=': 1415692800000,
+ *     '<=': 1418198400000
+ *   }
+ * }
+ * The only operator logic for combining filters that we support currently
+ * is "all". These simplified filters are converted to the syntax used by
+ * Mapbox https://www.mapbox.com/mapbox-gl-style-spec/#types-filter in
+ * the selector selectors/mapbox_filter.js
+ */
 const filters = (state = {}, {type, payload = {}}) => {
   const {key, exp, val} = payload
   switch (type) {
