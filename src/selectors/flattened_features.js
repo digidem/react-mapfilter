835b95a927bce45d2dd1641991811523af29202b
Seth Fitzsimmons <seth@mojodna.net>
2016-12-13 17:18:14 -0800

    Handle single-feature case
    With a single feature, there's no "best field", so there's no field to
    color by.
    
    This also updates the selector signature to match the order of selector
    dependencies.

--- src/selectors/flattened_features.js
+++ src/selectors/flattened_features.js
@@ -11,12 +11,15 @@ const getFlattenedFeatures = createSelector(
   getFeaturesWithIds,
   getColoredField,
   getColorIndex,
-  (features, colorIndex, coloredField) => {
+  (features, coloredField, colorIndex) => {
     return features.map(f => {
       const newProps = flat(f.properties, {safe: true})
-      const coloredFieldValue = newProps[coloredField]
-      const colorHex = Array.isArray(coloredFieldValue)
-        ? colorIndex[coloredFieldValue[0]] : colorIndex[coloredFieldValue]
+      let colorHex
+      if (coloredField) {
+        const coloredFieldValue = newProps[coloredField]
+        colorHex = Array.isArray(coloredFieldValue)
+          ? colorIndex[coloredFieldValue[0]] : colorIndex[coloredFieldValue]
+      }
       return assign({}, f, {
         properties: newProps,
         __color: (colorHex || CONFIG.defaultColor).slice(1)
