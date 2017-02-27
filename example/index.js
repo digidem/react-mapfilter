cec34e2f66654eec6cf385a3837afc09c8b0bf92
Gregor MacLennan <gmaclennan@digital-democracy.org>
2017-02-27 09:58:31 -0800

    Fix example (no brfs)

--- example/index.js
+++ example/index.js
@@ -1,12 +1,9 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const MapFilter = require('../src/index.js').default
-const fs = require('fs')
-const path = require('path')
 const createHistory = require('history').createBrowserHistory
 
-const sampleGeoJSON = fs.readFileSync(path.join(__dirname, './sample.geojson'), 'utf8')
-const features = JSON.parse(sampleGeoJSON).features
+const features = require('./sample.json').features
 
 const history = createHistory()
 
