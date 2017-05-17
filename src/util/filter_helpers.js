20446096d78e165013eca2fcaa3bebefc5438f47
Gregor MacLennan <gmaclennan@digital-democracy.org>
2017-05-17 09:26:36 -0400

    Fix date parsing for short dates (no time)

--- src/util/filter_helpers.js
+++ src/util/filter_helpers.js
@@ -4,12 +4,18 @@ import chrono from 'chrono-node'
 
 // const iso8601RegExp = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/
 
+const shortDateRegExp = /^(\d{4})-(\d{2})-(\d{2})$/
+
 /**
  * Attempt to parse a datestring, returning `false` if it can't be parsed
  * @param {string} possibleDate [description]
  * @return {date|boolean} returns a Date object or `false` if not a date.
  */
 export function parseDate (value) {
+  if (shortDateRegExp.test(value)) {
+    var m = value.match(shortDateRegExp)
+    value = (new Date(m[1], m[2] - 1, m[3])).toISOString()
+  }
   return isodate.parse(value) || chrono.strict.parseDate(value)
 }
 
