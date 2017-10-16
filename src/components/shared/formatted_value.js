1dd17f2de4013b087d9719004a2fb732db82b0b2
Gregor MacLennan <gmaclennan@digital-democracy.org>
2017-10-15 18:59:55 -0700

    Nice quotes for sexaguesimal coords

--- src/components/shared/formatted_value.js
+++ src/components/shared/formatted_value.js
@@ -40,7 +40,7 @@ function formatLocation (coords, format) {
     case FORMATS_DEC_DEG:
       return coords.map(coord => roundTo(coord, 5)).join(', ')
     case FORMATS_DEG_MIN_SEC:
-      return sexagesimal.formatPair({lon: coords[0], lat: coords[1]})
+      return sexagesimal.formatPair({lon: coords[0], lat: coords[1]}).replace(/'/g, '’').replace(/"/g, '”')
     case FORMATS_UTM:
       const utm = fromLatLon(coords[1], coords[0])
       return `X ${roundTo(utm.easting, 1)}, Y ${roundTo(utm.northing, 1)} — UTM ${utm.zoneNum}${utm.zoneLetter}`
