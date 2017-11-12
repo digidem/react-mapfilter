def514ce3e4213ce1def47b048361ccf127264b0
Gregor MacLennan <gmaclennan@digital-democracy.org>
2017-11-12 15:39:05 -0500

    Adjust FeatureDetail story

--- stories/feature_detail.js
+++ stories/feature_detail.js
@@ -13,22 +13,20 @@ const fieldAnalysis = require('./fixtures/field_analysis')
 const media = [{
   'fieldname': 'picture.url',
   'value': 'https://s3.amazonaws.com/images.digital-democracy.org/mapfilter-sample/sample-14.jpg'
-},
-{
+}, {
   'fieldname': 'picture2',
   'value': 'https://s3.amazonaws.com/images.digital-democracy.org/mapfilter-sample/sample-14.jpg'
-},
-{
+}, {
   'fieldname': 'picture3',
   'value': 'https://s3.amazonaws.com/images.digital-democracy.org/mapfilter-sample/sample-14.jpg'
 }]
 
-const store = createStore(state => state, {resizer: url => url})
+const store = createStore(state => state, {})
 
 storiesOf('FeatureDetail', module)
   .add('Default', () => (
     <Provider store={store}>
-      <div style={{maxWidth: 600, outline: '1px solid gray', margin: 'auto'}}>
+      <div style={{maxWidth: 600, margin: 'auto'}}>
         <FeatureDetail
           onRequestClose={action('requestClose')}
           onDeleteFeature={action('deleteFeature')}
