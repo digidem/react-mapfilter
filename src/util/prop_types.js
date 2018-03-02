d5acd2485b3968145659d51949ba23f94215e071
karissa <krmckelv@gmail.com>
2018-03-02 15:08:23 -0500

    accept booleans as well to the mapbox filter

--- src/util/prop_types.js
+++ src/util/prop_types.js
@@ -49,7 +49,8 @@ export const mapboxFilter = PropTypes.arrayOf(
       PropTypes.oneOfType([
         PropTypes.string,
         PropTypes.number,
-        PropTypes.array
+        PropTypes.array,
+        PropTypes.bool
       ])
     )
   ])
