f5d3812e8c632896ec8b8c9e5f5c8a51d567ca38
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-09-27 08:56:18 +0100

    feat: Add notes to example data

--- fixtures/transform.js
+++ fixtures/transform.js
@@ -2,6 +2,18 @@ const fc = require('./example_fc.json')
 const omit = require('lodash/omit')
 const fs = require('fs')
 const path = require('path')
+const LoremIpsum = require('lorem-ipsum').LoremIpsum
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 3,
+    min: 1
+  },
+  wordsPerSentence: {
+    max: 9,
+    min: 4
+  }
+})
 
 const observations = fc.features.map(f => ({
   id: f.id,
@@ -14,7 +26,10 @@ const observations = fc.features.map(f => ({
       id: randomId() + '.jpg',
       type: 'image/jpg'
     })),
-  tags: omit(f.properties, ['id', 'picture', 'pictures'])
+  tags: {
+    ...omit(f.properties, ['id', 'picture', 'pictures']),
+    notes: lorem.generateParagraphs(1)
+  }
 }))
 
 function randomInt(max) {
