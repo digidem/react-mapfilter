22a0adc6f586673706c23eac1755abc66efa42dd
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-10-01 19:14:01 -0500

    fix: Export FilterPanel

--- src/FilterPanel/index.js
+++ src/FilterPanel/index.js
@@ -1,15 +1,1 @@
-// @flow
-import React from 'react'
-
-import type { Observation } from 'mapeo-schema'
-import type { Filter } from '../types'
-
-type Props = {
-  filter: Filter,
-  setFilter: Filter => void,
-  observations: Observation[]
-}
-
-const FilterPane = ({ filter, setFilter, observations }: Props) => {}
-
-export default FilterPane
+export { default } from './FilterPanel'
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as ReportView } from './ReportView'
 export { default as MapView } from './MapView'
 export { default as MediaView } from './MediaView'
+export { default as FilterPanel } from './FilterPanel'
