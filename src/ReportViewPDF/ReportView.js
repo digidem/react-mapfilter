403d9f5b4aea46c749459ed0cabc3a02e6dae4d2
Gregor MacLennan <gmaclennan@digital-democracy.org>
2019-11-25 17:05:09 -0500

    WIP PDF reports

---
+++ src/ReportViewPDF/ReportView.js
@@ -0,0 +1,267 @@
+// @flow
+
+import React from 'react'
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Font
+} from '@react-pdf/renderer'
+import type { Observation } from 'mapeo-schema'
+import { FormattedTime, IntlProvider } from 'react-intl'
+
+// import ReportFeature from './ReportFeature'
+// import ReportPageContent from './ReportPageContent'
+import { defaultGetPreset, isEmptyValue } from '../utils/helpers'
+import { get } from '../utils/get_set'
+
+import type {
+  PaperSize,
+  CameraOptions,
+  CommonViewContentProps,
+  PresetWithAdditionalFields,
+  Primitive
+} from '../types'
+import FormattedFieldname from '../internal/FormattedFieldname'
+import FormattedValue from '../internal/FormattedValue'
+import FormattedLocation from '../internal/FormattedLocation'
+
+export type ReportViewPDFProps = {
+  /** Called with
+   * [CameraOptions](https://docs.mapbox.com/mapbox-gl-js/api/#cameraoptions)
+   * with properties `center`, `zoom`, `bearing`, `pitch` */
+  onMapMove?: CameraOptions => any,
+  /** Initial position of the map - an object with properties `center`, `zoom`,
+   * `bearing`, `pitch`. If this is not set then the map will by default zoom to
+   * the bounds of the observations. If you are going to unmount and re-mount
+   * this component (e.g. within tabs) then you will want to use onMove to store
+   * the position in state, and pass it as initialPosition for when the map
+   * re-mounts. */
+  initialMapPosition?: $Shape<CameraOptions>,
+  /** Mapbox access token */
+  mapboxAccessToken: string,
+  /** Mapbox style url */
+  mapStyle?: any
+}
+
+type Props = {
+  ...$Exact<ReportViewPDFProps>,
+  ...$Exact<CommonViewContentProps>,
+  /** Paper size for report */
+  paperSize?: PaperSize,
+  /** Render for printing (for screen display only visible observations are
+   * rendered, for performance reasons) */
+  print?: boolean
+}
+
+Font.register({
+  family: 'SourceSansPro',
+  fonts: [
+    { src: 'fonts/SourceSansPro-Regular.ttf' }, // font-style: normal, font-weight: normal
+    { src: 'fonts/SourceSansPro-Italic.ttf', fontStyle: 'italic' },
+    { src: 'fonts/SourceSansPro-Bold.ttf', fontWeight: 700 },
+    {
+      src: 'fonts/SourceSansPro-BoldItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 700
+    },
+    { src: 'fonts/SourceSansPro-Light.ttf', fontWeight: 300 },
+    {
+      src: 'fonts/SourceSansPro-LightItalic.ttf',
+      fontStyle: 'italic',
+      fontWeight: 300
+    }
+  ]
+})
+
+const FeaturePage = ({
+  observation,
+  preset = {}
+}: {
+  observation: Observation,
+  preset?: PresetWithAdditionalFields
+}) => {
+  const coords =
+    typeof observation.lon === 'number' && typeof observation.lat === 'number'
+      ? {
+          longitude: observation.lon,
+          latitude: observation.lat
+        }
+      : undefined
+  const createdAt =
+    typeof observation.created_at === 'string'
+      ? new Date(observation.created_at)
+      : undefined
+  const fields = preset.fields.concat(preset.additionalFields)
+  const tags = observation.tags || {}
+  const note = tags.note || tags.notes
+  return (
+    <Page size="A4" style={styles.page}>
+      <View style={styles.pageContent}>
+        <View style={styles.columnLeft}>
+          <Text style={styles.presetName}>{preset.name || 'Observation'}</Text>
+          {createdAt && (
+            <Text style={styles.createdAt}>
+              <Text style={styles.createdAtLabel}>Registrado: </Text>
+              <FormattedTime
+                key="time"
+                value={createdAt}
+                year="numeric"
+                month="long"
+                day="2-digit"
+              />
+            </Text>
+          )}
+          {coords && (
+            <Text style={styles.location}>
+              <Text style={styles.locationLabel}>Ubicación: </Text>
+              <FormattedLocation {...coords} />
+            </Text>
+          )}
+          {note &&
+            note.split('\n').map((para, idx) => (
+              <Text key={idx} style={styles.description}>
+                {para}
+              </Text>
+            ))}
+          <Text style={styles.details}>Detalles</Text>
+          {fields.map(field => {
+            const value: Primitive | Array<Primitive> = get(tags, field.key)
+            if (isEmptyValue(value)) return null
+            return (
+              <View key={field.id} style={styles.field} wrap={false}>
+                <Text style={styles.fieldLabel}>
+                  <FormattedFieldname field={field} />
+                </Text>
+                <Text style={styles.fieldValue}>
+                  <FormattedValue field={field} value={value} />
+                </Text>
+              </View>
+            )
+          })}
+        </View>
+        <View style={styles.columnRight}>
+          <View style={styles.map} />
+          {new Array(4).fill(null).map((att, i) => (
+            <View
+              key={i}
+              style={[
+                styles.image,
+                { height: Math.random() > 0.5 ? '70mm' : '50mm' }
+              ]}
+              wrap={false}
+            />
+          ))}
+        </View>
+      </View>
+    </Page>
+  )
+}
+
+export const PdfContext = React.createContext<boolean>(false)
+
+const ReportViewPDF = ({
+  observations,
+  getPreset = defaultGetPreset,
+  getMedia,
+  paperSize = 'a4',
+  mapboxAccessToken,
+  mapStyle
+}: Props) => {
+  return (
+    <PdfContext.Provider value={true}>
+      <IntlProvider>
+        <Document>
+          {observations.slice(0, 1).map(observation => (
+            <FeaturePage
+              key={observation.id}
+              observation={observation}
+              preset={getPreset(observation)}
+            />
+          ))}
+        </Document>
+      </IntlProvider>
+    </PdfContext.Provider>
+  )
+}
+
+export default ReportViewPDF
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: 'white',
+    padding: '15mm',
+    flexDirection: 'row'
+  },
+  pageContent: {
+    flex: 1,
+    flexDirection: 'row',
+    fontFamily: 'SourceSansPro'
+  },
+  columnLeft: {
+    flex: 2,
+    paddingRight: 12,
+    lineHeight: 1.2
+  },
+  columnRight: {
+    // backgroundColor: 'aqua',
+    flex: 1
+  },
+  presetName: {
+    fontWeight: 700
+  },
+  createdAt: {
+    fontSize: 12
+  },
+  createdAtLabel: {
+    fontSize: 12,
+    color: 'grey'
+  },
+  location: {
+    fontSize: 12,
+    marginBottom: 6
+  },
+  locationLabel: {
+    fontSize: 12,
+    color: 'grey'
+  },
+  map: {
+    height: '60mm',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 12,
+    backgroundColor: '#8E918B'
+  },
+  image: {
+    height: '50mm',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 12,
+    backgroundColor: '#C8D8E3'
+  },
+  description: {
+    marginBottom: 6,
+    fontSize: 12
+  },
+  details: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    marginBottom: 3,
+    marginTop: 12
+  },
+  field: {
+    marginBottom: 6
+  },
+  fieldLabel: {
+    fontSize: 9,
+    marginBottom: 1,
+    color: '#333333'
+  },
+  fieldValue: {
+    fontSize: 12
+  }
+})
--- src/internal/FormattedFieldname.js
+++ src/internal/FormattedFieldname.js
@@ -1,13 +1,14 @@
 // @flow
 import * as React from 'react'
 import { useIntl } from 'react-intl'
-
+import { Text } from '@react-pdf/renderer'
 import { getLocalizedFieldProp, fieldKeyToLabel } from '../utils/strings'
 import type { Field } from '../types'
+import { PdfContext } from '../ReportViewPDF/ReportView'
 
 const styles = {
   groupText: {
-    color: 'rgba(0, 0, 0, 0.541176)'
+    color: '#66666'
   }
 }
 
@@ -18,18 +19,20 @@ type Props = {
 /** Formats a field name nicely */
 const FormattedFieldname = ({ field }: Props) => {
   const { locale } = useIntl()
+  const isPdf = React.useContext(PdfContext)
+  const Component = isPdf ? Text : 'span'
   const label =
     getLocalizedFieldProp(field, 'label', locale) || fieldKeyToLabel(field.key)
   if (typeof label === 'string') {
-    return <span title={label}>{label}</span>
+    return <Component title={label}>{label}</Component>
   } else {
     const groupText = label.slice(0, label.length - 1).join(' / ') + ' / '
     const fieldText = label[label.length - 1]
     return (
-      <span title={groupText + fieldText}>
-        <span style={styles.groupText}>{groupText}</span>
-        <span>{fieldText}</span>
-      </span>
+      <Component title={groupText + fieldText}>
+        <Component style={styles.groupText}>{groupText}</Component>
+        <Component>{fieldText}</Component>
+      </Component>
     )
   }
 }
